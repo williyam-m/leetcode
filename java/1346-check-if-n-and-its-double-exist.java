ef3623c64ef410197e2ff201f2846e197065c156
williyam-m <williyam64@gmail.com>
2024-12-01 14:04:19 +0530

    create 1346-check-if-n-and-its-double-exist.java

---
+++ java/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int a : arr) {
+            if (set.contains(a * 2) || (set.contains(a / 2) && a % 2 == 0))
+               return true;
+            set.add(a);
+        }
+        return false;
+    }
+}
