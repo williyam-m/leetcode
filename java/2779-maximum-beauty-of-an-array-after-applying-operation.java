3f995fd139d6eb3d8311b9023f675f1f8246de0b
williyam-m <williyam64@gmail.com>
2024-12-11 10:17:32 +0530

    create 2779-maximum-beauty-of-an-array-after-applying-operation.java

---
+++ java/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        int ans = 0;
+        Arrays.sort(nums);
+        for (int l = 0, r = 0; r < nums.length; ++r) {
+            if (nums[r] - nums[l] > 2 * k)
+               ++l;
+            ans = Math.max(ans, r -l + 1);
+        }
+        return ans;
+    }
+}
