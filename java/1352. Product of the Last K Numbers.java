6085ac76e9aad1cb8d149387d60d6a94d99521ba
WILLIYAM M <williyam64@gmail.com>
2025-02-14 16:10:21 +0000

    create 1352. Product of the Last K Numbers.java

---
+++ java/1352. Product of the Last K Numbers.java
@@ -0,0 +1,24 @@
+class ProductOfNumbers {
+    private List<Integer> prefix = new ArrayList<>();
+    public ProductOfNumbers() {
+        prefix = new ArrayList<>(List.of(1));
+    }
+    
+    public void add(int num) {
+        if (num == 0)
+           prefix = new ArrayList<>(List.of(1));
+        else
+           prefix.add(prefix.get(prefix.size() - 1) * num);
+    }
+    
+    public int getProduct(int k) {
+        return k >= prefix.size() ? 0 : prefix.get(prefix.size() - 1) / prefix.get(prefix.size() - k - 1);
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
