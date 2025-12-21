2570b636bd6f6b3e2780d82c358db25673d883c4
williyam-m <williyam64@gmail.com>
2025-12-21 06:33:57 +0000

    create 944-delete-columns-to-make-sorted.java

---
+++ java/944-delete-columns-to-make-sorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int ans = 0;
+        for (int j = 0; j < strs[0].length(); ++j) {
+            for (int i = 0; i < strs.length - 1; ++i) {
+                if (strs[i].charAt(j) > strs[i + 1].charAt(j)) {
+                    ++ans;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
