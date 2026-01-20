5106e43da2bd06000170bd1dfb0e5de5c3929851
williyam-m <williyam64@gmail.com>
2026-01-20 20:27:27 +0000

    create 3314-construct-the-minimum-bitwise-array-i.java

---
+++ java/3314-construct-the-minimum-bitwise-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int[] result = new int[nums.size()];
+        for (int i = 0; i < nums.size(); ++i) {
+            result[i] = nums.get(i) == 2 ? -1 : nums.get(i) - getLeadingOneOfLastGroupOfOnes(nums.get(i));
+        }
+        return result;
+    }
+    private int getLeadingOneOfLastGroupOfOnes(int n) {
+        int leadingOne = 1;
+        while ((n & leadingOne) != 0) {
+            leadingOne <<= 1;
+        }
+        return leadingOne >> 1;
+    }
+}
