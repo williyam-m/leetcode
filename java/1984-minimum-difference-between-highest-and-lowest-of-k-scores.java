9e688c93cbb736399c14a5c5f64a26e31aa2c47c
williyam-m <williyam64@gmail.com>
2026-01-25 06:41:34 +0000

    create 1984-minimum-difference-between-highest-and-lowest-of-k-scores.java

---
+++ java/1984-minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i <= nums.length - k; ++i) {
+            minDiff = Math.min(minDiff, nums[i + k - 1] - nums[i]);
+        }
+        return minDiff;
+    }
+}
