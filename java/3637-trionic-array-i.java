5f3984318c329b8bc64533211c4d5c1057a925de
williyam-m <williyam64@gmail.com>
2026-02-03 18:58:21 +0000

    update 3637-trionic-array-i.java

--- java/3637-trionic-array-i.java
+++ java/3637-trionic-array-i.java
@@ -1,34 +1,27 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-        int i = 0, n = nums.length;
-        int prev = i;
-        i = findToIndexOfStrictlyInc(nums, i, n);
-        if (prev == i) {
-            return false;
-        }
-        prev = i;
-        i = findToIndexOfStrictlyDec(nums, i, n);
-        if (prev == i) {
-            return false;
-        }
-        prev = i;
-        i = findToIndexOfStrictlyInc(nums, i, n);
-        return prev != i && i == n - 1;
+        int n = nums.length;
+        int p = findNextDescIndex(nums, 0);
+        if (p == 0) { return false; }
+        int q = findNextIncIndex(nums, p);
+        if (p == q || q == n - 1) { return false; }
+        return findNextDescIndex(nums, q) == n - 1;
     }
-    public int findToIndexOfStrictlyDec(int[] nums, int i, int n) {
-        for (; i < n - 1; ++i) {
-            if (nums[i] <= nums[i + 1]) {
-                break;
-            }
-        }
-        return i;
-    }
-
-    public int findToIndexOfStrictlyInc(int[] nums, int i, int n) {
-        for (; i < n - 1; ++i) {
+    private int findNextDescIndex(int[] nums, int i) {
+        while (i < nums.length - 1) {
             if (nums[i] >= nums[i + 1]) {
                 break;
             }
+            ++i;
+        }
+        return i;
+    }
+    private int findNextIncIndex(int[] nums, int i) {
+        while (i < nums.length - 1) {
+            if (nums[i] <= nums[i + 1]) {
+                break;
+            }
+            ++i;
         }
         return i;
     }
