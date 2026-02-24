f1c3ec28844f005c46ff93f686a1892f31aa15f3
williyam-m <williyam64@gmail.com>
2026-02-24 19:51:02 +0000

    create 1022-sum-of-root-to-leaf-binary-numbers.java

---
+++ java/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+    private int dfs(TreeNode root, int bit) {
+        if (root == null) {
+            return 0;
+        }
+        bit = (bit * 2) + root.val;
+        if (root.left == null && root.right == null) {
+            return bit;
+        }
+        return dfs(root.left, bit) + dfs(root.right, bit);
+    }
+}
