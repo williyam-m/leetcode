04582c67bd564567f0795485d7ddd18d2a23ea7c
williyam-m <williyam64@gmail.com>
2026-04-08 16:14:16 +0000

    update 3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java

--- java/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ java/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,25 +1,18 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        int x = 0;
+        long ans = 0;
+        long tens = 1;
         int sum = 0;
         while (n > 0) {
-            int digit = (n % 10);
-            if (digit != 0) {
-                x *= 10;
-                x += digit;
-                sum += digit;
+            int mod = n % 10;
+            if (mod != 0) {
+                ans = tens * mod + ans;
+                sum += mod;
+                tens *= 10;
             }
             n /= 10;
         }
-        return (long) sum * reverse(x);
-    }
-    private int reverse(int num) {
-        int ans = 0;
-        while (num > 0) {
-            ans *= 10;
-            ans += num % 10;
-            num /= 10;
-        }
-        return ans;
+
+        return ans * sum;
     }
 }
