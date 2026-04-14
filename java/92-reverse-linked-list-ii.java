89e9284caa14595a98478ad3ab980f244a7e7d0c
williyam-m <williyam64@gmail.com>
2026-04-14 11:49:20 +0000

    create 92-reverse-linked-list-ii.java

--- java/92-reverse-linked-list-ii.java
+++ java/92-reverse-linked-list-ii.java
@@ -10,35 +10,21 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        int i = 1;
-        ListNode curr = head;
-        ListNode prev = null;
-        while (curr != null && left > i++) {
-            prev = curr;
-            curr = curr.next;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prevNode = dummy;
+
+        for (int i = 1; i < left; ++i) {
+            prevNode = prevNode.next;
         }
-        ListNode res[] = reverse(curr, right - left + 1);
-        if (left == 1) {
-            head = res[0];
-            if (res[1] == null)
-                return res[0];
+        ListNode currNode = prevNode.next;
+
+        for (int i = 1; i < right - left + 1; ++i) {
+            ListNode nextNode = currNode.next;
+            currNode.next = nextNode.next;
+            nextNode.next = prevNode.next;
+            prevNode.next = nextNode;
         }
-        else
-            prev.next = res[0];
-        curr.next = res[1];
-        
-        return head;
+        return dummy.next;
     }
-    private ListNode[] reverse (ListNode node, int len) {
-        ListNode curr = node;
-        ListNode prev = null;
-        ListNode next = null;
-        while (curr != null && len-- > 0) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return new ListNode[] {prev, curr};
-    } 
 }
