48c22449f203ead3167ba07451d51c5c388d6513
williyam-m <williyam64@gmail.com>
2026-05-24 09:03:09 +0000

    update 735-asteroid-collision.java

--- java/735-asteroid-collision.java
+++ java/735-asteroid-collision.java
@@ -2,22 +2,27 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
         for (int asteroid : asteroids) {
-            while (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0
-                    && Math.abs(stack.peek()) < Math.abs(asteroid)) {
-                stack.pop();
-            }
-            if (!stack.isEmpty() && (stack.peek() > 0 && asteroid < 0) && stack.peek() + asteroid == 0) {
-                stack.pop();
-                continue;
-            }
-            if (stack.isEmpty() || stack.peek() < 0 || asteroid > 0)
+            if (asteroid > 0) {
                 stack.push(asteroid);
+            }
+            else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -asteroid) {
+                    stack.pop();
+                }
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(asteroid);
+                }
+                else if (stack.peek() == -asteroid) {
+                    stack.pop();
+                }
+                // else : stack.peek() > asteroid
+            }
         }
 
         int[] ans = new int[stack.size()];
-        int i = 0;
-        for (int s : stack)
-            ans[i++] = s;
+        for (int i = stack.size() - 1; i >= 0; --i) {
+            ans[i] = stack.pop();
+        }
         return ans;
     }
 }
