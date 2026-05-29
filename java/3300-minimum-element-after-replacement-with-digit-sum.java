17254afbab9d72542f9e03a85e77bf52c570a568
williyam-m <williyam64@gmail.com>
2026-05-29 03:52:38 +0000

    Create 3300-minimum-element-after-replacement-with-digit-sum.java

--- java/3300-minimum-element-after-replacement-with-digit-sum.java
+++ java/3300-minimum-element-after-replacement-with-digit-sum.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minElement(int[] nums) {
         int ans = Integer.MAX_VALUE;
-        for (int num : nums) {
-            ans = Math.min(ans, findSum(num));
+        for (int n : nums) {
+            ans = Math.min(ans, sumOfDigits(n));
         }
         return ans;
     }
-    private int findSum (int n) {
-        int sum = 0;
-        while (n != 0) {
-            sum += n % 10;
+    private int sumOfDigits(int n) {
+        int ans = 0;
+        while (n > 0) {
+            ans += n % 10;
             n /= 10;
         }
-        return sum;
+        return ans;
     }
 }
