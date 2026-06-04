a92c177dcf3fc15908c43450adc62bb1d5060d66
williyam-m <williyam64@gmail.com>
2026-06-04 19:51:45 +0000

    update 347-top-k-frequent-elements.java

--- java/347-top-k-frequent-elements.java
+++ java/347-top-k-frequent-elements.java
@@ -1,19 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums)
-            map.merge(num, 1, Integer::sum);
-
-        Queue<Pair<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-        for (Map.Entry<Integer, Integer> m : map.entrySet()) {
-            minHeap.offer(new Pair<>(m.getKey(), m.getValue()));
-            if (minHeap.size() > k)
-                minHeap.poll();
-        }
-
         int[] ans = new int[k];
-        for (Pair<Integer, Integer> heap : minHeap)
-            ans[--k] = heap.getKey();
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            if (freq.containsKey(num)) {
+                freq.put(num, freq.get(num) + 1);
+            }
+            else {
+                freq.put(num, 1);
+            }
+        }
+        Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            minHeap.offer(new int[] {entry.getKey(), entry.getValue()});
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        for (int i = k - 1; i >= 0; --i) {
+            ans[i] = minHeap.poll()[0];
+        }
         return ans;
     }
 }
