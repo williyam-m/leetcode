50f3713bc4acf2ebb096872ed61d054836f3e27a
williyam-m <williyam64@gmail.com>
2026-06-19 12:02:36 +0000

    create 1732-find-the-highest-altitude.java

---
+++ java/1732-find-the-highest-altitude.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int altitude = 0, maxAltitude = 0;
+        for (int i = 0; i < gain.length; ++i) {
+            altitude += gain[i];
+            maxAltitude = Math.max(maxAltitude, altitude);
+        }
+        return maxAltitude;
+    }
+}
