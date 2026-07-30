19ec6daecc2a557328e03365378d3eb1da01bb05
williyam-m <williyam64@gmail.com>
2026-07-30 19:57:22 +0000

    create 3014-minimum-number-of-pushes-to-type-word-i.java

--- java/3014-minimum-number-of-pushes-to-type-word-i.java
+++ java/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,13 +1,34 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] count = new int[26];
-        for (char c : word.toCharArray())
-            ++count[c - 'a'];
-        Arrays.sort(count);
+        int n = word.length();
+        int groups = ((n - 1) / 8) + 1;
+        int ans = 4 * groups * (groups - 1);
+        return ans + (n - ((groups - 1) * 8)) * groups; // + remaining
+
+        /* solution 1"
         int ans = 0;
-        for (int i = 0; i < 26; ++i) {
-            ans += (count[25 - i] * (i / 8 + 1));
+        for (int i = 0; i < word.length(); ++i) {
+            ans += (i / 8) + 1;
         }
         return ans;
+        */
+
+        /* solution 2 : "not distinct"
+        if all letters in word are not distinct
+        int n = 26;
+        int[] count = new int[n];
+        for (char c : word.toCharArray()) {
+            ++count[c - 'a'];
+        }
+        Arrays.sort(count);
+        int ans = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            if (count[i] == 0) {
+                break;
+            }
+            ans += count[i] * (((n - 1 - i) / 8) + 1);
+        }
+        return ans;
+        */
     }
 }
