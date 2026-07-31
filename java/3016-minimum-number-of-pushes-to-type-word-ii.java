7fd4f10d23256a6cd9328757cdad3015dba43bf9
williyam-m <williyam64@gmail.com>
2026-07-31 03:57:52 +0000

    create 3016-minimum-number-of-pushes-to-type-word-ii.java

--- java/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ java/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,12 +1,17 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] count = new int[26];
-        for (char c : word.toCharArray())
+        int n = 26;
+        int[] count = new int[n];
+        for (char c : word.toCharArray()) {
             ++count[c - 'a'];
+        }
         Arrays.sort(count);
         int ans = 0;
-        for (int i = 0; i < 26; ++i) {
-            ans += (count[25 - i] * (i / 8 + 1));
+        for (int i = n - 1; i >= 0; --i) {
+            if (count[i] == 0) {
+                break;
+            }
+            ans += count[i] * (((n - 1 - i) / 8) + 1);
         }
         return ans;
     }
