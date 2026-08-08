d67d5c7e41fa8636ed2124e713428d234ed6af51
williyam-m <williyam64@gmail.com>
2026-08-08 09:34:10 +0000

    update 386-lexicographical-numbers.java

--- java/386-lexicographical-numbers.java
+++ java/386-lexicographical-numbers.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> ans = new ArrayList<Integer>();
-        int ele = 1;
-        while (ans.size() < n) {
-            ans.add(ele);
-            if (ele * 10 <= n) {
-                ele *= 10;
-            } else {
-                while (ele == n || ele % 10 == 9) {
-                    ele /= 10;
+        List<Integer> ans = new ArrayList<>();
+        int currNumber = 1;
+        for (int i = 0; i < n; ++i) {
+            ans.add(currNumber);
+            if (currNumber * 10 <= n) {
+                currNumber *= 10;
+            }
+            else {
+                while (currNumber >= n || currNumber % 10 == 9) {
+                    currNumber /= 10;
                 }
-                ++ele;
+                ++currNumber;
             }
         }
         return ans;
