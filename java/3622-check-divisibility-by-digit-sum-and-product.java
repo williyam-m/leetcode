43dfadb617110394fc2a279d4302a60c11ab7b7d
williyam-m <williyam64@gmail.com>
2026-08-22 17:46:44 +0000

    create 3622-check-divisibility-by-digit-sum-and-product.java

--- java/3622-check-divisibility-by-digit-sum-and-product.java
+++ java/3622-check-divisibility-by-digit-sum-and-product.java
@@ -1,21 +1,33 @@
 class Solution {
     public boolean checkDivisibility(int n) {
-        return n % (findSum(n) + findProduct(n)) == 0;
+        int sum = 0;
+        int product = 1;
+        int num = n;
+        while (num > 0) {
+            sum += num % 10;
+            product *= num % 10;
+            num /= 10;
+        }
+        return n % (sum + product) == 0;
     }
-    private int findSum (int n) {
-        int ans = 0;
+    /*
+    public boolean checkDivisibility(int n) {
+        return n % (find(n, '+') + find(n, '*')) == 0;
+    }
+    private int find(int n, char opr) {
+        int ans = opr == '+' ? 0 : 1;
         while (n > 0) {
-            ans += n % 10;
+            switch (opr) {
+                case '+':
+                    ans += n % 10;
+                    break;
+                case '*':
+                    ans *= n % 10;
+                    break;
+            }
             n /= 10;
         }
         return ans;
     }
-    private int findProduct (int n) {
-        int ans = 1;
-        while (n > 0) {
-            ans *= n % 10;
-            n /= 10;
-        }
-        return ans;
-    }
+    */
 }
