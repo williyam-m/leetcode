3f56887e87b19577fcf9244a3978d0e81dab2055
williyam-m <williyam64@gmail.com>
2026-09-08 19:02:01 +0000

    update 560-subarray-sum-equals-k.java

---
+++ java/560-subarray-sum-equals-k.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int ans = 0;
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; ++i) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return ans;
+        /*
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int sum = 0;
+            for (int j = i; j < n; ++j) {
+                sum += nums[j];
+                if (sum == k) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+        */
+    }
+}
