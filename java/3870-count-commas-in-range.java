e6f1f2c1364567b2de187af2c56dbc06e0dd08c3
williyam-m <williyam64@gmail.com>
2026-09-08 05:53:52 +0000

    update 3870-count-commas-in-range.java

--- java/3870-count-commas-in-range.java
+++ java/3870-count-commas-in-range.java
@@ -1,15 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        if (n <= 999) {
+        if (n < 1000) {
             return 0;
         }
-        int ans = 0;
-        int i = (String.valueOf(n).length() - 1) / 3;
-        n -= 999;
-        while (i > 0) {
-            ans += i-- * n;
-            n -= 999;
-        }
-        return ans;
+        return n - 999;
     }
 }
