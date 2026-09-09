ef5c82b446ea49b4fd802fce8b621e706b130814
williyam-m <williyam64@gmail.com>
2026-09-09 19:56:42 +0000

    create 3871-count-commas-in-range-ii.java

---
+++ java/3871-count-commas-in-range-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        long p = 1000;
+        while (p <= n) {
+            ans += n - p + 1;
+            p *= 1000;
+        }
+        return ans;
+    }
+}
