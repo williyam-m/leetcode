586b9f1b92bf54d6c11cc6d3b7ccd6698ddd4472
williyam-m <williyam64@gmail.com>
2026-09-15 16:43:11 +0000

    create 4048-count-values-with-equally-spaced-occurrences-i.java

---
+++ java/4048-count-values-with-equally-spaced-occurrences-i.java
@@ -0,0 +1,50 @@
+class CountValue {
+    int count = 0;
+    int lastIndex = -1;
+    int distance = -1;
+    boolean inValid = false;
+    CountValue(int index) {
+        lastIndex = index;
+        count = 1;
+    }
+}
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        HashMap<Integer, CountValue> map = new HashMap<>();
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], new CountValue(i));
+                continue;
+            }
+            CountValue cv = map.get(nums[i]);
+            if (cv.inValid == true) {
+                continue;
+            }
+            ++cv.count;
+
+            if (cv.distance == -1) {
+                cv.distance = i - cv.lastIndex;
+                cv.lastIndex = i;
+                continue;
+            }
+            else if (cv.distance != i - cv.lastIndex) {
+                if (cv.count > 3) {
+                    --ans;
+                }
+                cv.inValid = true;
+                continue;
+            }
+            cv.lastIndex = i;
+            if (cv.count == 3) {
+                ++ans;
+            }
+            else if (cv.count > 3) {
+                --ans;
+                cv.inValid = true;
+            }
+        }
+        return ans;
+    }
+}
